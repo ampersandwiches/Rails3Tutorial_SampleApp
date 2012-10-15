eb746a42a5b378c00c35cd5285bfd9b47baf10d2
Adam Price <adamhowardprice@gmail.com>
2012-10-15 01:26:09 -0400

    Added exercises from chapter 6

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -106,4 +106,14 @@ describe User do
     end
   end
 
+  describe "email address with mixed case" do
+  	let(:mixed_case_email) { "Foo@eXaMpLe.CoM" }
+
+  	it "should be saved as all lower-case" do
+  		@user.email = mixed_case_email
+  		@user.save
+  		@user.reload.email.should == mixed_case_email.downcase
+  	end
+  end
+
 end
